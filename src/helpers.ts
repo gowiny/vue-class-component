312bf25c25fb6dc54dc3c76c4c0040d18b0c4584
Terry <terry@gowiny.com>
2021-08-05 22:56:53 +0800

    解决多重继承后，createDecorator 出错的问题

--- src/helpers.ts
+++ src/helpers.ts
@@ -36,6 +36,10 @@ export function createDecorator(
         : (target.constructor as VueConstructor)
     if (!Ctor.__d) {
       Ctor.__d = []
+      Ctor.__d.__n = Ctor.name
+    }else if(Ctor.__d.__n !== Ctor.name){
+      Ctor.__d = [].concat(Ctor.__d)
+      Ctor.__d.__n = Ctor.name
     }
     if (typeof index !== 'number') {
       index = undefined
